c0abf2631316722d023878fabe63f96a642664f4
itashdv <itashcb@gmail.com>
2023-03-27 12:10:04 +0300

    Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Express Shina',
